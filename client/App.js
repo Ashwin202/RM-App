ad4f1c4e5b24dc3cc7ba27af84cbda7008bbb725
Nithin <nithin@askerbot.com>
2023-06-21 19:07:37 +0530

    Add Login UI

--- client/App.js
+++ client/App.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Dashboard from './components/Dashboard/Dashboard';
-import LoginForm from './components/LoginForm/LoginForm.js';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Dashboard from "./components/Dashboard/Dashboard";
+import LoginForm from "./components/LoginForm/LoginForm.js";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Image } from "react-native";
+import { Text, IconButton } from "@react-native-material/core";
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer independent={true}>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginForm} />
-        <Stack.Screen name="Dashboard" component={Dashboard} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer independent={true}>
+			<Stack.Navigator screenOptions={{ headerShown: false }}>
+				<Stack.Screen 
+					name="Login"
+					component={LoginForm}
+					
+				/>
+				<Stack.Screen
+					name="Dashboard"
+					component={Dashboard}
+					options={({ navigation, route }) => ({
+            headerShown: true,
+						headerLeft: () => (
+							<Image
+								source={require("./assets/logo.png")}
+								style={{ height:20, width:20,marginLeft: 10}}
+							/>
+						),
+						headerTitle: (props) => (
+							<Text variant="h6" style={{ marginLeft: 2}}>
+								Dashboard
+							</Text>
+						),
+						headerRight: () => (
+							<>
+								<IconButton
+									icon={(props) => <Icon name="ellipsis-v" {...props} />}
+								/>
+							</>
+						),
+					})}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
 
 export default App;
--- client/components/Dashboard/Dashboard.js
+++ client/components/Dashboard/Dashboard.js
@@ -1,26 +1,34 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import CustomDial from '../CustomDial/CustomDial'
 import LoginPage from '../LoginForm/LoginForm'
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-function SettingsScreen() {
+function InboxScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
+            <Text>Inbox content</Text>
         </View>
     );
 }
 
-const Tab = createBottomTabNavigator();
+function ContactScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Contact content</Text>
+        </View>
+    );
+}
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function Dashboard() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator tabBarPosition="bottom" independent={true}>
             <Tab.Screen name="Custom Dial" options={{ tabBarIcon: ({ color, size }) => (<Icon name="phone" size={22} color="black" />) }} component={CustomDial} />
-            <Tab.Screen name="Inbox" options={{ tabBarIcon: ({ color, size }) => (<Icon name="inbox" size={22} color="black" />) }} component={SettingsScreen} />
-            <Tab.Screen name="Contacts" options={{ tabBarIcon: ({ color, size }) => (<Icon name="address-book" size={22} color="black" />) }} component={LoginPage} />
+            <Tab.Screen name="Inbox" options={{ tabBarIcon: ({ color, size }) => (<Icon name="inbox" size={22} color="black" />) }} component={InboxScreen} />
+            <Tab.Screen name="Contacts" options={{ tabBarIcon: ({ color, size }) => (<Icon name="address-book" size={22} color="black" />) }} component={ContactScreen} />
         </Tab.Navigator>
     );
 }
